ed0d8a79c4db0b0db8a450e78ce415ebf641a79a
Brendon Gomes <brendonbachie@hotmail.com>
2026-01-24 20:39:06 -0300

    esta makeando

--- push_swap/turco.c
+++ push_swap/turco.c
@@ -2,16 +2,12 @@
 
 void    turco(t_elements **stack_a, t_elements **stack_b)
 {
-    int size;
     t_elements *min_custo;
 
-    size = ft_lstsize(&stack_a);
-
     pb(stack_a, stack_b);
-
     while (ft_lstsize(stack_a) > 3)
     {
-        index(*stack_a);
+        indice(*stack_a);
         index_b(*stack_a, *stack_b);
         alvo(*stack_a, *stack_b);
         custo(*stack_a, *stack_b);
@@ -22,7 +18,7 @@ void    turco(t_elements **stack_a, t_elements **stack_b)
     three(stack_a);
     while(stack_b)
     {
-        index(*stack_b);
+        indice(*stack_b);
         index_b(*stack_b, *stack_a);
         volta(*stack_a, *stack_b);
         custo(*stack_b, *stack_a);
--- push_swap/volta.c
+++ push_swap/volta.c
@@ -1,17 +1,17 @@
 #include "push_swap.h"
 
-void    volta(t_elements **stack_a, t_elements **stack_b)
+void    volta(t_elements *stack_a, t_elements *stack_b)
 {
     t_elements *temp_a;
     t_elements *temp_b;
     t_elements *alvo;
 
-    temp_a = *stack_a;
-    temp_b = *stack_b;
+    temp_a = stack_a;
+    temp_b = stack_b;
     while (temp_b)
     {
         alvo = NULL;
-        temp_a = *stack_a;
+        temp_a = stack_a;
         while (temp_a)
         {
             if (temp_b->number < temp_a->number && (alvo == NULL || temp_a->number < alvo->number))
