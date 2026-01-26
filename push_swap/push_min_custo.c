c8f4a613bfc5de86b4b8401aa2ebc1642823375d
bgomes-b <brendonbachie@hotmail.com>
2026-01-26 16:27:32 -0300

    push pronta

--- push_swap/push_min_custo.c
+++ push_swap/push_min_custo.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-t_node	*lowest_cost(t_node *stack_a)
+t_node	*lowest_cost_a(t_node *stack_a)
 {
 	t_node	*low_cost;
 	t_node	*temp;
@@ -21,11 +21,38 @@ t_node	*lowest_cost(t_node *stack_a)
 	low_cost = stack_a;
 	while (temp)
 	{
-		if (temp->total_cost < low_cost->total_cost)
+		if (temp->total_cost < low_cost->total_cost && ((temp->cost_a > 0 && temp->cost_b > 0)
+				|| (temp->cost_a < 0 && temp->cost_b < 0)))
+			low_cost = temp;
+		else if (temp->total_cost < low_cost->total_cost)
 			low_cost = temp;
 		if (temp->total_cost == low_cost->total_cost)
 		{
 			if (temp->number < low_cost->number)
+				low_cost = temp;
+		}	
+		temp = temp->next;
+	}
+	return (low_cost);
+}
+
+t_node	*lowest_cost_b(t_node *stack_b)
+{
+	t_node	*low_cost;
+	t_node	*temp;
+
+	temp = stack_b;
+	low_cost = stack_b;
+	while (temp)
+	{
+		if (temp->total_cost < low_cost->total_cost && ((temp->cost_a > 0 && temp->cost_b > 0)
+				|| (temp->cost_a < 0 && temp->cost_b < 0)))
+			low_cost = temp;
+		else if (temp->total_cost < low_cost->total_cost)
+			low_cost = temp;
+		if (temp->total_cost == low_cost->total_cost)
+		{
+			if (temp->number > low_cost->number)
 				low_cost = temp;
 		}	
 		temp = temp->next;
