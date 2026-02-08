6d7e69d402722de5cc08e962509ff3f258b47131
Brendon Gomes Bachie <brendonbachie@hotmail.com>
2026-02-08 20:53:44 -0300

    python

---
+++ Python 00/ex3/ft_plant_age.py
@@ -0,0 +1,6 @@
+def ft_plant_age():
+	day = int(input("Enter plant age in days: "))
+	if (day > 60)
+		printf("Plant is ready to harvest!")
+	else
+		printf("Plant needs more time to grow.")
---
+++ Python 00/ex4/ft_water_reminder.py
@@ -0,0 +1,6 @@
+def ft_water_reminder():
+	day = int(input("Days since last watering: "))
+	if (day > 2)
+		printf("Water the plants!")
+	else
+		printf("Plants are fine")
