443cd556e9cf9a54ba87fd534bca067698b774a8
bgomes-b <brendonbachie@hotmail.com>
2026-05-02 17:29:27 -0300

    ex0/1 prontos

--- Python/Python 05/ex0/stream_processor.py
+++ Python/Python 05/ex0/stream_processor.py
@@ -98,21 +98,82 @@ class LogProcessor(DataProcessor):
                 self._counter += 1
 
 
-def main() -> None:
+def main_num() -> None:
     print("Testing NumericProcessor...")
 
-    number = 42
-    number_list = [1, 2, 3]
+    number_list = [1, 2, 3, 4, 5]
     num_processor = NumericProcessor()
 
-    print(f" Trying to validate input '{number}': {num_processor.validate(number)}")
-    if num_processor.validate(number):
-        num_processor.ingest(number)
-    if num_processor.validate(number_list):
-        num_processor.ingest(number_list)
-    print(num_processor.output())
-    print(num_processor.output())
+    print(f" Trying to validate input '42': {num_processor.validate(42)}")
+    print(f" Trying to validate input 'Hello': {num_processor.validate('Hello')}")
+    print(f" Test invalid ingestion of string 'foo'without prior validation:")
+    try:
+        num_processor.ingest("foo")
+    except ValueError as e:
+        print(f" Got exception: {e}")
+
+    print(f"Processing data: {number_list}")
+    for item in number_list:
+        if num_processor.validate(item):
+            num_processor.ingest(item)
+
+    print("Extracting 3 values")
+    for _ in range(3):
+        try:
+            index, value = num_processor.output()
+            print(f" Numeric value {index}: {value}")
+        except ValueError as e:
+            print(f" Got exception: {e}")
+
+
+def main_text() -> None:
+    print("Testing TextProcessor...")
+
+    text_list = ["Hello", "Nexus", "World"]
+    text_processor = TextProcessor()
+
+    print(f" Trying to validate input '42': {text_processor.validate(42)}")
+
+    print(f"Processing data: {text_list}")
+    for item in text_list:
+        if text_processor.validate(item):
+            text_processor.ingest(item)
+
+    print("Extracting 1 value")
+    for _ in range(1):
+        try:
+            index, value = text_processor.output()
+            print(f" Text value {index}: {value}")
+        except ValueError as e:
+            print(f" Got exception: {e}")
+
+
+def main_log() -> None:
+    print("Testing LogProcessor...")
+
+    log_list = [
+        {"log_level": "NOTICE", "log_message": "Connection to server"},
+        {"log_level": "ERROR", "log_message": "Unauthorized access"},
+    ]
+    log_processor = LogProcessor()
+
+    print(f" Trying to validate input 'Hello': {log_processor.validate('Hello')}")
+
+    print(f"Processing data: {log_list}")
+    for item in log_list:
+        if log_processor.validate(item):
+            log_processor.ingest(item)
+
+    print("Extracting 2 value")
+    for _ in range(2):
+        try:
+            index, value = log_processor.output()
+            print(f" Log entry {index}: {value}")
+        except ValueError as e:
+            print(f" Got exception: {e}")
 
 
 if __name__ == "__main__":
-    main()
+    main_num()
+    main_text()
+    main_log()
