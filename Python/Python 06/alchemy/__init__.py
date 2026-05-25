3e72a000da3ff8a9c46fa464b1a28392618cab29
bgomes-b <brendonbachie@hotmail.com>
2026-05-25 18:28:00 -0300

    codexion

--- Python/Python 06/alchemy/__init__.py
+++ Python/Python 06/alchemy/__init__.py
@@ -1,8 +1,12 @@
-from .potions import healing_potion, strength_potion
+from .potions import healing_potion as heal, strength_potion
 from .elements import create_air
+from . import transmutation
+from .transmutation.recipes import lead_to_gold
 
 __all__ = [
-    'create_air',
-    'healing_potion',
-    'strength_potion'
+    "create_air",
+    "heal",
+    "strength_potion",
+    "transmutation",
+    "lead_to_gold",
 ]
---
+++ Python/Python 06/alchemy/grimoire/dark_validator.py
@@ -0,0 +1,12 @@
+from .dark_spellbook import dark_spell_allowed_ingredients  # type: ignore
+
+
+def validate_ingredients(ingredients: str) -> str:
+    allowed = dark_spell_allowed_ingredients()
+    ingredients_lower = ingredients.lower()
+
+    for item in allowed:
+        if item in ingredients_lower:
+            return f"{ingredients} -> VALID"
+
+    return f"{ingredients} -> INVALID"
